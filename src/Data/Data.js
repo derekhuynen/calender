ed7589c701062f53cb6eadde365319603804eb47
derekhuynen <Derek.huynen@gmail.com>
2022-03-27 17:55:36 -0700

    Good Start

---
+++ src/Data/Data.js
@@ -0,0 +1,3 @@
+export const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+export const Days = {Sunday: "Su", Monday: "Mo", Tuesday: "Tu" , Wednesday: "We", Thursday: "Th", Friday: "Fr", Saturday: "Sa"}
+
---
+++ src/pages/Calender.js
@@ -0,0 +1,101 @@
+import React, {useState} from 'react'
+import {Months,Days} from "../Data/Data";
+
+import {Icon} from '@iconify/react';
+import leftArrowCircle from '@iconify/icons-bxs/left-arrow-circle';
+import rightArrowCircle from '@iconify/icons-bxs/right-arrow-circle';
+import '../Css/Calender.css';
+
+
+function getDay(year, month, day){
+    return new Date(year, month, day).getDay()
+}
+
+function getDaysInMonth(year, month) {
+    const temp = new Date(year, (month === 11 ? 0 : month + 1), 0);
+    console.log(temp)
+
+    return temp.getDate()
+
+}
+
+const createHeaders = () =>{
+    return Object.entries(Days).map((day,index) => {
+        return(
+            <div className={"days-header"}>
+                <span>{day[1]}</span>
+            </div>)
+    })
+}
+
+const createDays = (year, month) =>{
+    const result = []
+
+    const dayOfWeek = getDay(year, month, 1)
+    const daysInMonth = getDaysInMonth(year, month);
+    const previousMonth = ((month === 0 ? getDaysInMonth(year, 11) :  getDaysInMonth(year, month)))
+
+    for (let i = previousMonth; i > previousMonth - dayOfWeek; i--) {
+        result.push(
+            <div className={"extra"}>
+                <span>{i}</span>
+            </div>
+        )
+    }
+
+    for (let i = 1; i < daysInMonth + 1; i++) {
+        result.push(
+            <div className={"days"}>
+                <span>{i}</span>
+            </div>
+        )
+    }
+
+    for (let i = 1; i < (42 - daysInMonth - dayOfWeek + 1); i++) {
+        result.push(
+            <div className={"extra"}>
+                <span>{i}</span>
+            </div>
+        )
+    }
+    return result
+}
+
+
+
+export default function Calender() {
+
+    const [month, setMonth] = useState(0);
+    const [year, setYear] = useState(2022);
+
+
+    const increaseMonth = () => {
+        setMonth((month === 11 ? 0 : month + 1))
+    }
+
+    const decreaseMonth = () => {
+        setMonth((month === 0 ? 11 : month - 1))
+    }
+
+    return (
+            <div className={"calender-container"}>
+                <div className={"calender-header"}>
+                    <h1>
+                        <Icon icon={leftArrowCircle} className={"arrow"} onClick={decreaseMonth}/>
+                        <div className={"headerName"}>{Months[month]}</div>
+                        <Icon icon={rightArrowCircle} className={"arrow"} onClick={increaseMonth}/>
+                    </h1>
+
+                    <h3>
+                        <Icon icon={leftArrowCircle} className={"arrow"} onClick={() => setYear(year - 1)}/>
+                        <div className={"headerName"}>{year}</div>
+                        <Icon icon={rightArrowCircle} className={"arrow"} onClick={() => setYear(year + 1)}/>
+                    </h3>
+                </div>
+                <div className={"calender-bottom"}>
+                    {createHeaders()}
+                    {createDays(year,month)}
+                </div>
+            </div>
+    )
+}
